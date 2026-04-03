a9e0851459a5f583bfac8f46342c53262f80812c
qvarkk <xumdes@gmail.com>
2026-04-04 01:50:28 +0500

    added factories, seeders and commands

---
+++ education/management/commands/seed_db.py
@@ -0,0 +1,38 @@
+# education/management/commands/seed_db.py
+import random
+
+from django.core.management.base import BaseCommand
+import factory
+from education.factories import (
+    CompanyFactory, CourseFactory, SpecificationFactory,
+    GroupFactory, EmployeeFactory, GroupEmployeeFactory
+)
+
+class Command(BaseCommand):
+    help = 'Заполнить базу данных тестовыми данными'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Генерация компаний...')
+        companies = CompanyFactory.create_batch(3)
+
+        self.stdout.write('Генерация учебных курсов...')
+        courses = CourseFactory.create_batch(5)
+
+        self.stdout.write('Генерация спецификаций...')
+        specs = [SpecificationFactory(company=company) for company in companies for _ in range(2)]
+
+        self.stdout.write('Генерация учебных групп...')
+        groups = GroupFactory.create_batch(10, specification=factory.Iterator(specs))
+
+        self.stdout.write('Генерация участников групп...')
+        employees = []
+        for company in companies:
+            employees.extend(EmployeeFactory.create_batch(10, company=company))
+
+        self.stdout.write('Генерация связей...')
+        for group in groups:
+            chosen = random.sample(employees, k=random.randint(3, 15))
+            for emp in chosen:
+                GroupEmployeeFactory(group=group, employee=emp)
+
+        self.stdout.write(self.style.SUCCESS('База данных заполнена успешно!'))
