fc5fed08e175d3474a7c671d4e29050b27c2b452
qvarkk <xumdes@gmail.com>
2026-04-04 21:48:51 +0500

    added gunicorn and nginx configurations

--- core/settings.py
+++ core/settings.py
@@ -20,14 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4famwdmkbi2mk5ic*kt7#jq1n^p)nn&*_u+u!w)^r*ti8oo@h%'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+STATIC_ROOT = '/app/staticfiles'
 
 # Application definition
 
